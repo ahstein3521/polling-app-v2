76f54ec1c1ebc008dcad0e4bd8a82153bc7f822e
ahstein3521 <adamhs3521@gmail.com>
2016-08-23 18:07:14 -0400

    added sass and updated styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
-var gulp=require("gulp");
-var concat = require('gulp-concat');
-var uglify= require("gulp-uglify");
+var gulp = require('gulp');
+var uglify=require("gulp-uglify");
+var server = require('gulp-express');
+var autoprefixer=require('gulp-autoprefixer')
+var plumber=require("gulp-plumber");
+var sourcemaps=require("gulp-sourcemaps")
+var sass=require("gulp-sass")
+
 
 gulp.task("scripts",function(){
-	return gulp.src(["public/scripts/_global.js","public/scripts/*.js"])
-	
-	.pipe(uglify()) //running this function after concat messes up file order although that order makes more logical sense
-	.pipe(concat("main.js"))
-	.pipe(gulp.dest("public/dist"))
-
+	gulp.src("public/scripts/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("public/dist"))
+		.pipe(server.notify())
 })
+
+gulp.task("styles",function(){
+	return gulp.src("public/styles/styles.scss")
+			.pipe(plumber(function(err){
+				console.log(err);
+				this.emit("end");
+			}))
+			.pipe(sourcemaps.init())
+			.pipe(autoprefixer())
+			.pipe(sass({outputStyle:"compressed"}))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest("public/dist"))
+			.pipe(server.notify())
+			
+})		
+
+
+
+gulp.task('server', function () {
+     
+    server.run(["app.js"]);
+
+    gulp.watch("public/scripts/*.js",["scripts"])
+    gulp.watch("public/styles/**/*.scss",['styles'])
+    gulp.watch(['app.js'], [server.run])
+    
+});
